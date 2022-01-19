5543064f2f868af3abc131039142c5a057207258
Kiril Komitski <k.komitski94@gmail.com>
2022-01-19 11:41:51 +0000

    html and css done

---
+++ api/getData.js
@@ -0,0 +1,54 @@
+export const getData = async () => {
+	const countriesAPI = 'https://restcountries.com/v3.1/all';
+	async function allCountriesAPI(url) {
+		try {
+			const countries = await fetch(url);
+			const response = await countries.json();
+
+			return response;
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	const data = await allCountriesAPI(countriesAPI);
+
+	function byPopulation() {
+		const sorter = (a, b) => parseInt(a.population) - parseInt(b.population);
+		return data.sort(sorter).reverse();
+	}
+
+	function byName() {
+		const sorter = (a, b) => {
+			if (a.name.common > b.name.common) {
+				return 1;
+			} else if (a.name.common < b.name.common) {
+				return -1;
+			} else {
+				return 0;
+			}
+		};
+		return data.sort(sorter);
+	}
+
+	function byContinent(continent) {
+		const sorter = (a, b) => {
+			if (a.name.common > b.name.common) {
+				return 1;
+			} else if (a.name.common < b.name.common) {
+				return -1;
+			} else {
+				return 0;
+			}
+		};
+		return data.filter((country) => country.region === continent).sort(sorter);
+	}
+
+	return {
+		byPopulation: byPopulation,
+		byName: byName,
+		byContinent: byContinent,
+	};
+};
+
+export const countries = await getData();
---
+++ app.js
@@ -0,0 +1,8 @@
+import { countries } from './api/getData.js';
+
+// console.log(countries.byPopulation());
+const searchBar = document.querySelector('.search');
+
+searchBar.addEventListener('scroll', (e) => console.log(e));
+
+searchBar.scrollTop;
